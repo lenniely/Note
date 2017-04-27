1503296b23848218863a7c2b8d2087c6da92a219
lenniely <lnkx12345@163.com>
2017-04-27 17:07:41 +0800

    Note

---
+++ React/Component interactive/Forms/input.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Input = React.createClass({
+  getInitialState: function(){
+    return {
+      userInput:''
+    };
+  },
+  handleUserInput : function(e){
+    this.setState({
+      userInput: e.target.value
+    });
+  },
+  render: function () {
+    return (
+      <div>
+        <input type="text" onChange={this.handleUserInput} value={this.state.userInput} />
+        <h1>{this.state.userInput}</h1>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+	<Input />,
+	document.getElementById('app')
+);
