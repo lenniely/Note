450835f244542b80c39c6db44b9ed496978e8707
xR <wyrdbm@163.com>
2017-06-27 22:07:56 +0800

    note

--- React/React小书/react-redux-execise/src/ThemeSwitch.js
+++ React/React小书/react-redux-execise/src/ThemeSwitch.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 class ThemeSwitch extends Component {
+	static contextTypes = {
+		store: PropTypes.object
+	};
+
+	constructor(){
+		super();
+		this.state = {themeColor: ''};
+	}
+
+	componentWillMount(){
+		_updateThemeColor();
+	}
+
+	_updateThemeColor(){
+		const store = this.context;
+		const state = store.getState();
+		this.setState({ themeColor : state.themeColor });
+	}
+
 	render () {
 		return (
 			<div>
-				<button>Red</button>
-				<button>Blue</button>
+				<button style={{this.state.themeColor}}>Red</button>
+				<button style={{this.state.themeColor}}>Blue</button>
 			</div>
 		);
 	}
--- React/React小书/react-redux-execise/src/index.js
+++ React/React小书/react-redux-execise/src/index.js
@@ -10,7 +10,42 @@ import './index.css';
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
+function createStore(reducer){
+	let state = null;
+	const listeners = [];
+	const subscriber = (listener) => listeners.push(listener);
+	const getState = () => state;
+	const dispatch = (action) => {
+		state = reducer(state, action);
+		listeners.forEach(listener => listener());
+	}
+	dispatch({});
+	return{getState, dispatch, subscriber};
+}
+
+const themeReducer = (state, action) => {
+	if(!state) return{
+		themeColor: 'red'
+	};
+	switch(action.type){
+		case 'CHANGE_COLOR':
+			return{...state, themeColor: action.themeColor};
+		default: 
+			return state;
+	}
+}
+
+const store = createStore(themeReducer);
+
 Class Index extends Component {
+	static childContextTypes = {
+		store: PropTypes.object
+	};
+
+	getChildContex(){
+		return {store};
+	}
+
 	render () {
 		return (
 			<div>
