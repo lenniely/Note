533f9be761ee2deab89d6bc35d38b232370bab6d
lenniely <lnkx12345@163.com>
2017-07-05 16:03:30 +0800

    use real redux react-redux

--- React/React小书/react-redux-execise/src/index.js
+++ React/React小书/react-redux-execise/src/index.js
@@ -6,23 +6,24 @@ import registerServiceWorker from './registerServiceWorker';
 import Header from './Header'
 import Content from './Content'
 import './index.css';
-import {Provider} from './react-redux'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+// //ReactDOM.render(<App />, document.getElementById('root'));
+// //registerServiceWorker();
 
-function createStore(reducer){
-	let state = null;
-	const listeners = [];
-	const subscribe = (listener) => listeners.push(listener);
-	const getState = () => state;
-	const dispatch = (action) => {
-		state = reducer(state, action);
-		listeners.forEach(listener => listener());
-	}
-	dispatch({});
-	return{getState, dispatch, subscribe};
-}
+// function createStore(reducer){
+// 	let state = null;
+// 	const listeners = [];
+// 	const subscribe = (listener) => listeners.push(listener);
+// 	const getState = () => state;
+// 	const dispatch = (action) => {
+// 		state = reducer(state, action);
+// 		listeners.forEach(listener => listener());
+// 	}
+// 	dispatch({});
+// 	return{getState, dispatch, subscribe};
+// }
 
 const themeReducer = (state, action) => {
 	if(!state) return{
